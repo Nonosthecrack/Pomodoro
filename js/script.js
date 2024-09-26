c70a25ef9457afad0dd1e1c121a0d10a37dd13ac
Noa-f <ferault.noa@gmail.com>
2024-09-26 17:06:52 +0200

    avncée javascipt

--- js/script.js
+++ js/script.js
@@ -1,5 +1,58 @@
-const buttonWork = document.getElementById("work");
-const buttonBrek = document.getElementById("break");
+let workTitle = document.getElementById("work");
+let breackTittle = document.getElementById("break");
 
 let work = 25;
 let pause = 5;
+
+let secondes = 0;
+
+function afficheTimer(temps) {
+  let chronoS = temps.toString();
+  chronoS = chronoS.length < 2 ? "0" + chronoS : chronoS;
+  return chronoS;
+}
+
+window.onload = () => {
+  document.getElementById("minutes").innerHTML = afficheTimer(work);
+  document.getElementById("secondes").innerHTML = afficheTimer(secondes);
+};
+
+function start() {
+  document.getElementById("start").style.display = "none";
+  document.getElementById("reset").style.display = "block";
+
+  secondes = 59;
+
+  let workM = work - 1;
+  let breakM = pause - 1;
+
+  breakCount = false;
+
+  let fonctionDecompte = () => {
+    document.getElementById("minutes").innerHTML = afficheTimer(workM);
+    document.getElementById("secondes").innerHTML = afficheTimer(secondes);
+
+    secondes = secondes - 1;
+
+    if (secondes === 0) {
+      workM = workM - 1;
+
+      if (workM === -1) {
+        if (!breakCount) {
+          workM = breakM;
+          breakCount = true;
+        } else {
+          workM = work - 1;
+          breakCount = false;
+        }
+      }
+      secondes = 59;
+    }
+  };
+
+  setInterval(afficheTimer, 1000);
+}
+
+function reset() {
+  location.reload();
+}
