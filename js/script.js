7f2b226c37c42ff33fd1e2a048d714c1218a040f
Noa-f <ferault.noa@gmail.com>
2024-09-28 16:51:44 +0200

    avancement js

--- js/script.js
+++ js/script.js
@@ -1,60 +1,84 @@
-let workTitle = document.getElementById("work");
-let breackTittle = document.getElementById("break");
+let workTittle = document.getElementById("work");
+let breakTittle = document.getElementById("break");
 
-let minute = 25;
-let seconde = 0;
+let minute = 0;
+let seconde = 10;
 
-let minuteW = 25;
-let secondeW = 0;
+let minuteW = 0;
+let secondeW = 10;
 
-let minuteB = 5;
-let secondeB = 0;
+let minuteB = 0;
+let secondeB = 5;
 
+let pause = true;
+
+let id;
+
+//variable qui stock l'id boutton lancer
+let lancerBoutton = document.getElementById("start");
+let resetBoutton = document.getElementById("reset");
+
+//lance la fonction decompte quand le bouton est cliqué
+lancerBoutton.addEventListener("click", lancerDecompte);
+resetBoutton.addEventListener("click", reset);
+
+//fonction qui permetra l'affichage du temps
 function afficheTimer(temps) {
   let chronoS = temps.toString();
   chronoS = chronoS.length < 2 ? "0" + chronoS : chronoS;
   return chronoS;
 }
 
+//on affiche le temps et le bouton reset n'apparait pas
 window.onload = () => {
-  document.getElementById("minutes").innerHTML = afficheTimer(work);
-  document.getElementById("secondes").innerHTML = afficheTimer(secondes);
+  document.getElementById("reset").style.display = "none";
+  document.getElementById("minutes").innerHTML = afficheTimer(minute);
+  document.getElementById("secondes").innerHTML = afficheTimer(seconde);
 };
 
-function start() {
+function timer() {
   document.getElementById("start").style.display = "none";
   document.getElementById("reset").style.display = "block";
 
-  secondes = 59;
-
-  let workM = work - 1;
-  let breakM = pause - 1;
-
-  breakCount = false;
-
-  let fonctionDecompte = () => {
-    document.getElementById("minutes").innerHTML = afficheTimer(workM);
-    document.getElementById("secondes").innerHTML = afficheTimer(secondes);
-
-    secondes = secondes - 1;
-
-    if (secondes === 0) {
-      workM = workM - 1;
-
-      if (workM === -1) {
-        if (!breakCount) {
-          workM = breakM;
-          breakCount = true;
-        } else {
-          workM = work - 1;
-          breakCount = false;
-        }
-      }
-      secondes = 59;
+  if (seconde == 0 && minute != 0) {
+    seconde = 59;
+    minute -= 1;
+  } else {
+    seconde -= 1;
+  }
+  document.getElementById("minutes").innerHTML = afficheTimer(minute);
+  document.getElementById("secondes").innerHTML = afficheTimer(seconde);
+  if (seconde == 0 && minute == 0) {
+    if (pause) {
+      minute = minuteB;
+      seconde = secondeB;
+      workTittle.classList.remove("active");
+      breakTittle.classList.add("active");
+      document.getElementById("minutes").innerHTML = afficheTimer(minuteB);
+      document.getElementById("secondes").innerHTML = afficheTimer(secondeB);
+      pause = false;
+    } else {
+      minute = minuteW;
+      seconde = secondeW;
+      workTittle.classList.add("active");
+      breakTittle.classList.remove("active");
+      pause = true;
     }
-  };
+  }
+}
 
-  setInterval(afficheTimer, 1000);
+function lancerDecompte() {
+  minute = minuteW;
+  seconde = secondeW;
+  id = setInterval(timer, 1000);
+}
+
+function arretDecompte() {
+  clearInterval(id);
+  minute = minuteW;
+  seconde = secondeW;
+  document.getElementById("minutes").innerHTML = afficheTimer(minute);
+  document.getElementById("secondes").innerHTML = afficheTimer(seconde);
 }
 
 function reset() {
